2a3c36eb71815f088d7ea6a64362b0056b30600d
INX <115958651+dotinx@users.noreply.github.com>
2023-10-21 21:32:56 +0800

    Rewrite Code Structure

--- app.py
+++ app.py
@@ -1,29 +1,5 @@
 import random
 import sys
-filename_source = "source.mp4"
-filename_target = "target.mp4"
-buf_size = 114514
-record = False
-
-for i in range(1,len(sys.argv)):
-    if sys.argv[i] == "--record":
-        record = True
-    if i == len(sys.argv) - 1:
-        continue
-    if sys.argv[i] in ["--buffer","--randseed"]:
-        buf_size = int(str(sys.argv[i + 1]))
-    if sys.argv[i] in ["--source","--file","--from","--input","--in"]:
-        filename_source = str(sys.argv[i + 1])
-    if sys.argv[i] in ["--target","--output","--out"]:
-        filename_target = str(sys.argv[i + 1])
-
-source = open(filename_source,"rb")
-target = open(filename_target,"wb+")
-
-if record:
-    tgt_log = open(str(filename_source) + ".log","w+")
-
-k = 0
 
 def buffer_edit(buf : bytes) -> bytes:
     global k,tgt_log
@@ -34,12 +10,41 @@ def buffer_edit(buf : bytes) -> bytes:
     if record:
         tgt_log.write(str(k) +" " + str(random_edit_seed) +"\n")
     return buf
-
-while True:
-    buf = source.read(buf_size)
-    if buf:
-        target.write(buffer_edit(buf))
-    else:
-        break
-target.close()
-source.close()
+def main(argv : list) -> int:
+    global k,tgt_log
+    filename_source = "source.mp4"
+    filename_target = "target.mp4"
+    buf_size = 114514
+    record = False
+    
+    for i in range(1,len(argv)):
+        if sys.argv[i] == "--record":
+            record = True
+        if i == len(sys.argv) - 1:
+            continue
+        if sys.argv[i] in ["--buffer","--randseed"]:
+            buf_size = int(str(argv[i + 1]))
+        if sys.argv[i] in ["--source","--file","--from","--input","--in"]:
+            filename_source = str(argv[i + 1])
+        if sys.argv[i] in ["--target","--output","--out"]:
+            filename_target = str(argv[i + 1])
+    
+    source = open(filename_source,"rb")
+    target = open(filename_target,"wb+")
+    
+    if record:
+        tgt_log = open(str(filename_source) + ".log","w+")
+    
+    k = 0
+    
+    while True:
+        buf = source.read(buf_size)
+        if buf:
+            target.write(buffer_edit(buf))
+        else:
+            break
+    target.close()
+    source.close()
+    
+if __name__ == '__main__':
+    main(sys.argv)
